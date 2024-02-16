154daf4b523c1bc737e73c22b93ee72b3d71fb8b
Charlottedelignieres <charlottedelignieres@outlook.fr>
2024-02-16 15:01:44 +0100

    Version qui fonctionne avec une table produit et un champ ingrédient

--- NutriFood/settings.py
+++ NutriFood/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'NutriFood',
-        'USER': 'Charlotte',
+        'USER': 'charlotte',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '5432',
--- search/models.py
+++ search/models.py
@@ -10,3 +10,8 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class Produit(models.Model):
+    nom = models.CharField(max_length=100)
+    ingredient = models.TextField()
